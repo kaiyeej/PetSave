51abe70825ba5b354985673028740241a5d67bbd
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-10-28 15:02:46 +0800

    mobile

---
+++ mobile/core/variables.php
@@ -0,0 +1,10 @@
+<?php
+/** Core Path **/
+define("host","localhost");
+define("username","u981310152_ps_root");
+define("password","lMJKix7;3?");
+define("database","u981310152_petsave_db");
+
+//inside dir
+define ("VALUE",serialize (array("","my_functions.php")));
+?>

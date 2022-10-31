660ec3790ec9b43fdcc7174498f51e8eada32f6e
kaiyeej <58097285+kaiyeej@users.noreply.github.com>
2022-10-31 08:45:03 +0800

    Update modal_upload.php

--- admin/pages/animals/modal_upload.php
+++ admin/pages/animals/modal_upload.php
@@ -14,9 +14,9 @@
                         <i class="bx bx-x d-block d-sm-none"></i>
                         <span class="d-none d-sm-block">Close</span>
                     </button>
-                    <button type="submit" class="btn btn-primary ml-1" id="btn_submit">
+                    <button type="submit" class="btn btn-primary ml-1">
                         <i class="bx bx-check d-block d-sm-none"></i>
-                        <span class="d-none d-sm-block">Submit</span>
+                        <span class="d-none d-sm-block">Update</span>
                     </button>
                 </div>
             </div>

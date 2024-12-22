c3e0cbccef7751a104a277f5f3f23b06e72b9fd6
Kaye Jacildo <58097285+kaiyeej@users.noreply.github.com>
2024-12-22 15:18:28 +0800

    Update env.php

--- admin/core/env.php
+++ admin/core/env.php
@@ -4,9 +4,9 @@ define('BASE_URL', 'http://localhost/petsave/');
 define("BASE_PATH", __DIR__ . "/../");
 
 define("HOST", "localhost");
-define("USER", "root");
-define("PASSWORD", "");
-define("DBNAME", "petadoption_db");
+define("USER", "u814036432_pa_root");
+define("PASSWORD", "petAdoption123!");
+define("DBNAME", "u814036432_petadoption_db");
 
 /*define("HOST", "localhost");
 define("USER", "u981310152_ps_root");
